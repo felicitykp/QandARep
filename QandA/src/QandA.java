ee4e70e496c8338c78246979bc5a2891444b425c
felicitykp <felicitykp@users.noreply.github.com>
2020-09-16 13:57:05 -0400

    9/16

--- QandA/src/QandA.java
+++ QandA/src/QandA.java
@@ -1,4 +1,22 @@
-
 public class QandA {
 
+	public static void main(String[] args) throws InterruptedException {
+		
+		//name
+		System.out.println("What is your name?\n");
+		Thread.sleep(1500);
+		System.out.println("My name is Felicity, but I have a bunch wacky nicknames that have stuck around from my \n"
+				+ "freshman year. One of my classmates couldn't really pronounce my name, so I started getting \n"
+				+ "called everything from Faculty, Facilities, Velocity,Chastity, and really anything else that \n"
+				+ "kinda sounds like my name.\n");
+		Thread.sleep(7000);
+		
+		//from
+		System.out.println("Where are you from?\n");
+		Thread.sleep(1500);
+		System.out.println("I was born and raised in North Carolina right outside of Charlotte in the boonies. It \n"
+				+ "has stuck with me a little and a y'all will slip out every once in a while. But, for the last \n"
+				+ "three years I have lived in Wellesley, MA.\n");
+		
+	}
 }
